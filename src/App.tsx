09215fdf7a7d9261a8d909d2147cfc5995f95887
Jp <emricndayake@gmail.com>
2025-08-25 13:24:24 +0100

    Ajout du formulaire

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+type priorite = "Urgente" | "Moyenne" | "Basse";
+
+type Todo = {
+  id: number;
+  text: string;
+  priority: priorite;
+};
+
 function App() {
   return (
-    <div className="">
-      <button className="btn btn-accent">Delete</button>
+    <div className="flex justify-center">
+      <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
+        <div className="flex gap-4">
+          <input
+            type="text"
+            className="input w-full"
+            placeholder="Ajouter une tâche..."
+          />
+          <select className="select w-full" id="">
+            <option value="Urgente">Urgente</option>
+            <option value="Moyenne">Moyenne</option>
+            <option value="Basse">Basse</option>
+          </select>
+          <button className="btn btn-primary">Ajouter</button>
+        </div>
+      </div>
     </div>
   );
 }
