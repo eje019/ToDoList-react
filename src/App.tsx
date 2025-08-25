a66789dcc81443d6c7e3ac6b1b5b4cd4b99879b2
Jp <emricndayake@gmail.com>
2025-08-25 20:09:48 +0100

    Implementation de l'ajout des tâches

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type priorite = "Urgente" | "Moyenne" | "Basse";
 
@@ -11,6 +11,32 @@ type Todo = {
 function App() {
   const [input, setInput] = useState<string>("");
   const [priorite, setPriority] = useState<priorite>("Moyenne");
+  const savedTodos = localStorage.getItem("todos");
+  const initialTodos = savedTodos ? JSON.parse(savedTodos) : [];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  function addToDo() {
+    if (input.trim() == "") {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: input.trim(),
+      priority: priorite,
+    };
+
+    const newTodos = [newTodo, ...todos];
+    setTodos(newTodos);
+    setInput("");
+    setPriority("Moyenne");
+    console.log(newTodos);
+  }
+
   return (
     <div className="flex justify-center">
       <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
@@ -31,7 +57,9 @@ function App() {
             <option value="Moyenne">Moyenne</option>
             <option value="Basse">Basse</option>
           </select>
-          <button className="btn btn-primary">Ajouter</button>
+          <button onClick={addToDo} className="btn btn-primary">
+            Ajouter
+          </button>
         </div>
       </div>
     </div>
