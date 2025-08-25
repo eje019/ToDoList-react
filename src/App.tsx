1cd3b7f9f0122244139c760b3f2c909931c30d7e
Jp <emricndayake@gmail.com>
2025-08-25 19:23:13 +0100

    Ajout du onchange mettre à jour automatiquement les variables quand on tape quelque chose dans un input

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ type Todo = {
 
 function App() {
   const [input, setInput] = useState<string>("");
-  const [priority, setPriority] = useState<string>("Moyenne");
+  const [priorite, setPriority] = useState<priorite>("Moyenne");
   return (
     <div className="flex justify-center">
       <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
@@ -20,8 +20,13 @@ function App() {
             className="input w-full"
             placeholder="Ajouter une tâche..."
             value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
-          <select className="select w-full" value={priority}>
+          <select
+            className="select w-full"
+            value={priorite}
+            onChange={(e) => setPriority(e.target.value as priorite)}
+          >
             <option value="Urgente">Urgente</option>
             <option value="Moyenne">Moyenne</option>
             <option value="Basse">Basse</option>
